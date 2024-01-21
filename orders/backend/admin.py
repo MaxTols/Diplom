b2f0d1abfd11d39d27cd991f61599972f1e9a2bb
MaxTols <tolstikov-95@inbox.ru>
2024-01-21 14:15:20 +0300

    Create admin

--- orders/backend/admin.py
+++ orders/backend/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Shop, Category, Product, ProductInfo, Order, OrderItem, Contact
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(ProductInfo)
+class ProductInfoAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    ...
--- orders/backend/serializers.py
+++ orders/backend/serializers.py
@@ -3,13 +3,6 @@ from rest_framework import serializers
 from .models import User, Shop, Category, Product, ProductInfo, Parameter, ProductParameter, Order, OrderItem, Contact
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'surname', 'name', 'email',)
-        read_only_fields = ('id',)
-
-
 class ShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shop
@@ -18,56 +11,78 @@ class ShopSerializer(serializers.ModelSerializer):
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    shop = ShopSerializer(read_only=True)
+
     class Meta:
         model = Category
-        fields = ('id', 'name',)
+        fields = ('id', 'name', 'shop')
         read_only_fields = ('id',)
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+
     class Meta:
         model = Product
-        fields = ('id', 'name')
+        fields = ('id', 'name', 'category')
         read_only_fields = ('id',)
 
 
 class ProductInfoSerializer(serializers.ModelSerializer):
+    shop = ShopSerializer(read_only=True)
+    product = ProductSerializer(read_only=True)
+
     class Meta:
         model = ProductInfo
-        fields = ('id', 'name', 'quantity', 'price', 'price_rrc')
+        fields = ('id', 'name', 'shop', 'product', 'quantity', 'price', 'price_rrc')
         read_only_fields = ('id',)
 
 
-class ParameterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Parameter
-        fields = ('id', 'name')
-        read_only_fields = ('id',)
-
-
-class ProductParameterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductParameter
-        fields = ('id', 'name')
-        read_only_fields = ('id',)
-
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ('id', 'dt', 'status',)
-        read_only_fields = ('id',)
-
-
-class OrderItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrderItem
-        fields = ('id', 'name')
-        read_only_fields = ('id',)
+# class ParameterSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Parameter
+#         fields = ('id', 'name')
+#         read_only_fields = ('id',)
+#
+#
+# class ProductParameterSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ProductParameter
+#         fields = ('id', 'value')
+#         read_only_fields = ('id',)
 
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = ('id', 'phone', 'city', 'street', 'house', 'building', 'structure', 'apartment',)
+        read_only_fields = ('id',)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    contacts = ContactSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'surname', 'name', 'email', 'contacts')
+        read_only_fields = ('id',)
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ('id', 'dt', 'status', 'user')
+        read_only_fields = ('id',)
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    order = OrderSerializer(read_only=True)
+    product_info = ProductInfoSerializer(read_only=True)
+    shop = ShopSerializer(read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ('id', 'order', 'shop', 'product_info', 'quantity')
         read_only_fields = ('id',)
